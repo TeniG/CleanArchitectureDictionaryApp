4df2531b9b23e826ebac6c205e823885ff7ffbf6
TeniGada <teni@mobisuo.com>
2023-12-07 17:14:28 +0530

    Dao and Database created

---
+++ app/src/main/java/com/tenig/cleanarchitecturedictionaryapp/feature_dictionary/data/local/WordInfoDao.kt
@@ -0,0 +1,20 @@
+package com.tenig.cleanarchitecturedictionaryapp.feature_dictionary.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.tenig.cleanarchitecturedictionaryapp.feature_dictionary.data.local.entity.WordInfoEntity
+
+@Dao
+interface WordInfoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWordInfos(wordInfo: List<WordInfoEntity>)
+
+    @Query("Delete from word_info where word IN (:words)")
+    fun deleteWordInfos(words: List<String>)
+
+    @Query("Select * from word_info where word LIKE '%' || :word || '%'")
+    fun getWordInfos(word: String) : List<WordInfoEntity>
+}
