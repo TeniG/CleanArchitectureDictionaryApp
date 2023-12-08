b9a53f4634eeb6f72f519da7d9f4ad2618fe833f
TeniGada <teni@mobisuo.com>
2023-12-08 11:54:36 +0530

    Di added, AppModule and App created

---
+++ app/src/main/java/com/tenig/cleanarchitecturedictionaryapp/DictionaryApp.kt
@@ -0,0 +1,9 @@
+package com.tenig.cleanarchitecturedictionaryapp
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DictionaryApp : Application() {
+}
